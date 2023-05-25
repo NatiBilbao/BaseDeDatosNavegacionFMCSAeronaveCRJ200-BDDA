1d3fc28a4fcf755aeceaabaec5cc2edbc5b7549a
NatyBilbao <natabilbaocano18@gmail.com>
2023-05-24 23:48:42 -0400

    Proyecto de Base de datos avanzada Primer Parcial

---
+++ scripts/Tablas1.sql
@@ -0,0 +1,84 @@
+-- Tabla "Usuarios"
+CREATE TABLE Usuarios (
+  ID_usuario INT PRIMARY KEY,
+  Nombre VARCHAR(50),
+  Apellido VARCHAR(50),
+  Nombre_de_usuario VARCHAR(50),
+  Contraseña VARCHAR(50),
+  Nivel_de_acceso INT
+);
+
+-- Tabla "Aeropuertos"
+CREATE TABLE Aeropuertos (
+  ID_aeropuerto INT PRIMARY KEY,
+  Nombre VARCHAR(100),
+  Ciudad VARCHAR(100),
+  País VARCHAR(100),
+  Latitud DECIMAL(9,6),
+  Longitud DECIMAL(9,6)
+);
+
+-- Tabla "Rutas"
+CREATE TABLE Rutas (
+  ID_ruta INT PRIMARY KEY,
+  Aeropuerto_origen INT,
+  Aeropuerto_destino INT,
+  Distancia FLOAT,
+  Duración_estimada TIME,
+  Velocidad_promedio FLOAT,
+  FOREIGN KEY (Aeropuerto_origen) REFERENCES Aeropuertos(ID_aeropuerto),
+  FOREIGN KEY (Aeropuerto_destino) REFERENCES Aeropuertos(ID_aeropuerto)
+);
+
+-- Tabla "Vuelos"
+CREATE TABLE Vuelos (
+  ID_vuelo INT PRIMARY KEY,
+  ID_ruta INT,
+  Fecha DATE,
+  Hora_salida TIME,
+  Hora_llegada TIME,
+  Número_de_pasajeros INT,
+  FOREIGN KEY (ID_ruta) REFERENCES Rutas(ID_ruta)
+);
+
+-- Tabla "Aeronaves"
+CREATE TABLE Aeronaves (
+  ID_aeronave INT PRIMARY KEY,
+  Modelo VARCHAR(100),
+  Fabricante VARCHAR(100),
+  Año_de_fabricación INT,
+  Capacidad_de_pasajeros INT,
+  Peso_máximo_despegue FLOAT
+);
+
+-- Tabla "Tripulación"
+CREATE TABLE Tripulación (
+  ID_tripulación INT PRIMARY KEY,
+  ID_vuelo INT,
+  ID_piloto INT,
+  ID_copiloto INT,
+  ID_auxiliar INT,
+  FOREIGN KEY (ID_vuelo) REFERENCES Vuelos(ID_vuelo),
+  FOREIGN KEY (ID_piloto) REFERENCES Usuarios(ID_usuario),
+  FOREIGN KEY (ID_copiloto) REFERENCES Usuarios(ID_usuario),
+  FOREIGN KEY (ID_auxiliar) REFERENCES Usuarios(ID_usuario)
+);
+
+-- Tabla "Pasajeros"
+CREATE TABLE Pasajeros (
+  ID_pasajero INT PRIMARY KEY,
+  ID_vuelo INT,
+  Nombre VARCHAR(50),
+  Apellido VARCHAR(50),
+  Asiento VARCHAR(10),
+  FOREIGN KEY (ID_vuelo) REFERENCES Vuelos(ID_vuelo)
+);
+
+-- Tabla "Bitácora"
+CREATE TABLE Bitácora (
+  ID_bitácora INT PRIMARY KEY,
+  ID_vuelo INT,
+  Registro_fecha_hora DATETIME,
+  Evento VARCHAR(100),
+  FOREIGN KEY (ID_vuelo) REFERENCES Vuelos(ID_vuelo)
+);
