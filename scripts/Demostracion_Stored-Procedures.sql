d4efb623389d8301a06460462e17d6b9623cd76b
NatiBilbao <49278336+NatiBilbao@users.noreply.github.com>
2023-06-14 19:55:35 -0400

    Update Demostracion_Stored-Procedures.sql

--- scripts/Demostracion_Stored-Procedures.sql
+++ scripts/Demostracion_Stored-Procedures.sql
@@ -1,3 +1,5 @@
+USE Nata;
+
 CALL InsertarAeronave(8, 'Boeing 747', 'Boeing', '747', 416, 14285.7);
 CALL ObtenerInfoPilotos();
 CALL ActualizarDetallesVuelo(1, '2023-06-14', '08:00:00', '12:00:00', 'Nueva York', 'Los Ángeles');
